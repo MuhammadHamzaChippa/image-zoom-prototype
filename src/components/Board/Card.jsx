9bebb458344c716c6e77d24ea24b0d49cf3d87e4
mhamzachippa <mhamzachippa@gmail.com>
2023-06-25 17:22:15 +0500

    Keep the Scale Same while resizing the cards

--- src/components/Board/Card.jsx
+++ src/components/Board/Card.jsx
@@ -3,31 +3,38 @@ import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 import { Resizable } from "re-resizable";
 
 const Card = ({ card }) => {
-	const [height, setHeight] = useState(103);
-	const [width, setWidth] = useState(182);
+	const initialHeight = 103;
+	const initialWidth = 182;
+	const [height, setHeight] = useState(initialHeight);
+	const [width, setWidth] = useState(initialWidth);
+	const [scale, setScale] = useState({ x: 1, y: 1 });
 	return (
 		<Resizable
 			className="absolute bg-[white] w-fit rounded-[8px] h-fit"
 			style={{ left: card.x, top: card.y }}
 			size={{ width, height }}
 			onResizeStop={(e, direction, ref, d) => {
-				setWidth(width + d.width);
-				setHeight(height + d.height);
+				const newWidth = width + d.width;
+				const newHeight = height + d.height;
+				setScale({ x: initialWidth / newWidth, y: initialHeight / newHeight });
+				setWidth(newWidth);
+				setHeight(newHeight);
 			}}
 		>
-				<TransformWrapper>
-					<TransformComponent>
-						<img
-							style={{
-								height: height,
-								width: width,
-							}}
-							src={card.image}
-							alt={card.id}
-							className="rounded-[8px]"
-						/>
-					</TransformComponent>
-				</TransformWrapper>
+			<TransformWrapper>
+				<TransformComponent>
+					<img
+						style={{
+							height: height,
+							width: width,
+							transform: `scale(${scale.x}, ${scale.y})`,
+						}}
+						src={card.image}
+						alt={card.id}
+						className="rounded-[8px]"
+					/>
+				</TransformComponent>
+			</TransformWrapper>
 		</Resizable>
 	);
 };
